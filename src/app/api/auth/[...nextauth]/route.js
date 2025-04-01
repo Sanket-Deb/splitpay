2314b3dd3d8bbdd6ddff32b23ca5fceea69e903d
Sanket Deb <imsanketdeb20@gmail.com>
2025-04-02 00:38:47 +0530

    adding google provider for sign in with google

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,8 +1,8 @@
 import NextAuth from "next-auth";
 import { SupabaseAdapter } from "@next-auth/supabase-adapter";
 import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
 import { supabase } from "@/lib/supabase";
-import { adapter } from "next/dist/server/web/adapter";
 
 export const authOptions = {
   providers: [
@@ -13,20 +13,34 @@ export const authOptions = {
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials) {
+        // Authenticate user using Supabase
         const { email, password } = credentials;
         const { data, error } = await supabase.auth.signInWithPassword({
           email,
           password,
         });
         if (error) throw new Error(error.message);
-        return data.user;
+        return data.user; //returning authenticated user data
       },
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
     }),
   ],
   adapter: SupabaseAdapter({
     url: process.env.NEXT_PUBLIC_SUPABASE_URL,
     secret: process.env.SUPABASE_SERVICE_ROLE_KEY,
   }),
+  callbacks: {
+    async session({ session, token }) {
+      session.user.id = token.sub; // Attach user ID to session
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/login", // custom login page
+  },
   secret: process.env.NEXTAUTH_SECRET,
 };
 
