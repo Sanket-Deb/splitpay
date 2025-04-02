1bd6721acc959ec833563ff9ae4e1af22e77341d
Sanket Deb <imsanketdeb20@gmail.com>
2025-04-02 16:41:31 +0530

    login page comeplete build

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,23 +1,51 @@
 "use client";
-
-import { signIn, signOut, useSession } from "next-auth/react";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { signIn } from "next-auth/react";
+import { supabase } from "@/lib/supabase";
 
 const LoginPage = () => {
-  const { data: session } = useSession();
-  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  useEffect(() => {
-    if (session) {
-      router.push("/home"); //redirect to home if already signed in
-    }
-  }, [session, router]);
+  //for signin with email-password
+  const handleLogin = async () => {
+    await signIn("credentials", {
+      email,
+      password,
+      redirect: true,
+      callbackUrl: "/home",
+    });
+  };
+
+  //for signin with google
+  const handleGoogleSignIn = async () => {
+    await signIn("google", { redirect: true, callbackUrl: "/home" });
+  };
+
+  //for new user registration
+  const handleSignUp = async () => {
+    const { error } = await supabase.auth.signUp({ email, password });
+    if (error) console.log(error.message);
+  };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1>Sign in to SplitPay</h1>
-      <button onClick={() => signIn("google")}>Sign in with Google</button>
+    <div>
+      <h2> Login / Sign Up</h2>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
+      <button onClick={handleGoogleSignIn}>Sign in with Google</button>
+      <button onClick={handleSignUp}>Sign up</button>
     </div>
   );
 };
